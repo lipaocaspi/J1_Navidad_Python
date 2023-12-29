94667fa93d107150257f96bfc4f3edba2debcac8
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-28 19:55:35 -0500

    update_menu

---
+++ 03_intermedio/menu.py
@@ -0,0 +1,11 @@
+menuP = "1. Registrar Productos\n2. Visualizar Productos\n3. Actualizar Stock\n4. Informe de Productos Críticos\n5. Ganancia Potencial\n6. Salir"
+
+def menuShow() -> int:
+    global hasError
+    hasError = True
+    print(menuP)
+    while (hasError):
+        try:
+            return int(input(f"Ingrese una opción : "))
+        except ValueError:
+            hasError = True
