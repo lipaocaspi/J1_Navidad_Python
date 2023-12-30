d02d87cc5235c383595f3f7b5e3759275ada1c89
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-30 12:21:21 -0500

    update_format

--- 01_principiante.py
+++ 01_principiante.py
@@ -1,21 +1,34 @@
-# SUMATORIA DE 3 NÚMEROS POSITIVOS
+# SUMATORIA DE 3 NÚMEROS ENTEROS POSITIVOS
+
+import os
 
 nums = []
 isNegative = True
 suma = 0
 
+header = """
+****************************************
+SUMATORIA DE 3 NÚMEROS ENTEROS POSITIVOS
+****************************************
+"""
+os.system("cls")
+print(header)
+
 while (isNegative):
     try:
-        num = int(input("Ingrese el número : "))
+        num = int(input(f"Ingrese el número : "))
     except ValueError:
-        print("Ingrese un dato válido")
+        print(f"Ingrese un dato válido")
     else:
         if (num >= 0):
             nums.append(num)
         else:
-            print("Ingrese un número entero positivo")
+            print(f"Ingrese un número entero positivo")
     if (len(nums) == 3):
         isNegative = False
         suma = sum(nums)
 
-print("La sumatoria de los números es", suma)
+print(f"")
+print(f"La sumatoria de los números es : {suma}")
+
+os.system("pause")
