3268a976658993bfca245aa6ae2c352ca0cae053
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-30 12:33:40 -0500

    update_format

--- 02_principiante.py
+++ 02_principiante.py
@@ -3,9 +3,9 @@
 import os
 
 header = """
-*******************************
+**************************************
 \tCÁLCULO IMC
-*******************************
+**************************************
 """
 
 def verify(valorDato, nombreDato, tipoDato):
@@ -15,15 +15,17 @@ def verify(valorDato, nombreDato, tipoDato):
         try:
             valorDato = tipoDato(input(f"Ingrese {nombreDato} del estudiante : "))
         except ValueError:
-            print("Ingrese un dato válido")
+            print(f"Ingrese un dato válido")
         else:
             if (valorDato <= 0):
-                print("Ingrese un número positivo")
+                print(f"Ingrese un número positivo")
             else:
                 isIncorrect = False
     return valorDato
 
-nombre = input("Ingrese el nombre del estudiante : ")
+os.system("cls")
+
+nombre = input(f"Ingrese el nombre del estudiante : ")
 valor = 0
 edad = verify(valor, "edad", int)
 peso = verify(valor, "peso", float)
@@ -45,8 +47,12 @@ elif (imc > 40):
     estudiante[5] = "Obesidad III"
 
 os.system("cls")
+
 print(header)
-print("Nombre:    \t", estudiante[0])
-print("Edad:      \t", estudiante[1])
-print("IMC:       \t", estudiante[4])
-print("Categoría: \t", estudiante[5])
+print(f"Nombre:    \t{estudiante[0]}")
+print(f"Edad:      \t{estudiante[1]}")
+print(f"IMC:       \t{estudiante[4]}")
+print(f"Categoría: \t{estudiante[5]}")
+print(f"")
+
+os.system("pause")
